4e5145713221f233d37ea33900963229d67aa33c
Nguyễn Ngọc Thịnh <thinh23022004@gmail.com>
2026-03-13 00:18:34 +0700

    chore: setup .env for frontend and backend, update .gitignore, and project structure improvements

---
+++ backend/src/main/java/vn/edu/hcmuaf/fit/fashionstore/FashionstoreApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.fashionstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionstoreApplication.class, args);
+	}
+
+}
